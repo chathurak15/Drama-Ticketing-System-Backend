365cca23f5072e30b1233ef93563ad99e56f4235
chathura <chathurakavindu15@gmail.com>
2026-01-05 21:58:15 +0530

    changed security config

--- src/main/java/com/example/NatakaLK/config/SecurityConfig.java
+++ src/main/java/com/example/NatakaLK/config/SecurityConfig.java
@@ -98,7 +98,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of(frontendBaseUrl));
+        config.setAllowedOrigins(List.of("http://localhost:5173", frontendBaseUrl));
         config.setAllowCredentials(true);
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
